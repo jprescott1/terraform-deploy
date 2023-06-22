ca55bd0150bce5286abaad2e8c342f0c6d96d125
Jimmy P <bunches_stands_0s@icloud.com>
2023-06-22 10:29:44 -0500

    GLOOG-1: apply is failing on ssh-key. Try fingerprint instead

--- main.tf
+++ main.tf
@@ -16,6 +16,6 @@ resource "digitalocean_droplet" "www-1" {
   region = "nyc3"
   size   = "s-1vcpu-1gb"
   ssh_keys = [
-    data.digitalocean_ssh_key.terraform.name
+    data.digitalocean_ssh_key.terraform.fingerprint
   ]
 }
