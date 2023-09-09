3e647b5871b0734a10662f44d955192da67ebe39
Jimmy Prescott <bunches_stands_0s@icloud.com>
2023-09-09 14:07:19 -0500

    deploy droplet

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ terraform {
   }
 }
 
+resource "digitalocean_droplet" "www-1" {
+  image  = "ubuntu-20-04-x64"
+  name   = "www-1"
+  region = "nyc3"
+  size   = "s-1vcpu-1gb"
+  ssh_keys = [
+    data.digitalocean_ssh_key.terraform.fingerprint
+  ]
+}
+
 data "digitalocean_ssh_key" "terraform" {
   name = "m1-mac"
 }
