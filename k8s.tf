120e174865532721036f1ac71d56948a8666487f
Jimmy Prescott <bunches_stands_0s@icloud.com>
2024-05-21 18:20:43 -0500

    fix format

--- k8s.tf
+++ k8s.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_kubernetes_cluster" "terra-sfo3" {
-  name   = "terra-sfo3"
-  region = "sfo3"
+  name    = "terra-sfo3"
+  region  = "sfo3"
   version = "1.29.1-do.0"
 
   node_pool {
