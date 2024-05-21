2a35f9c640896c7fcfd2e02996e8e9699806e851
Jimmy Prescott <bunches_stands_0s@icloud.com>
2024-05-21 18:17:34 -0500

    create 3 node cluster

---
+++ k8s.tf
@@ -0,0 +1,11 @@
+resource "digitalocean_kubernetes_cluster" "terra-sfo3" {
+  name   = "terra-sfo3"
+  region = "sfo3"
+  version = "1.29.1-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-4gb"
+    node_count = 3
+  }
+}
