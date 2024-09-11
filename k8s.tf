6dcd60644e446a4ae6f06736f445f86115753b16
Jimmy Prescott <bunches_stands_0s@icloud.com>
2024-09-10 22:03:38 -0500

    change datacenter

--- k8s.tf
+++ k8s.tf
@@ -1,6 +1,6 @@
 resource "digitalocean_kubernetes_cluster" "terra-sfo3" {
-  name    = "terra-sfo3"
-  region  = "sfo3"
+  name    = "terra-nyc1"
+  region  = "nyc1"
   version = "1.30.4-do.0"
 
   node_pool {
